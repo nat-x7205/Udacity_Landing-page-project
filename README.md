7f12a4f7bc821dfb7d270e6aa0a35c36804693c0
Natalia Petrenko <petrenkonataly@gmail.com>
2021-07-26 22:17:39 +1000

    Add scrollTo functionality to menu link items

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 # Udacity_Landing-page-project
-The goal of this project to practice manipulating DOM using JavaScript.
-This is a multi-section landing page, with a dynamically updating navigational menu based on the amount of content that is added to the page.
+This project is a part of the Front Eng Web Developer nanodegree program. The goal of this project is to practice manipulating DOM using JavaScript.
 
-## Tools used
+The project requirements are to build a multi-section landing page, with a dynamically updating navigational menu based on the amount of content that is added to the page.
+
+## Languages used
 - HTML;
 - CSS;
 - JavaScript.
 
 ## Project requirements
-- **Usability:** all features are usable across modern desktop, tablet, and phone browsers.
-- **Navigation:** navigation is built dynamically as an unordered list.
-- **Section Active State:** it should be clear which section is being viewed while scrolling through the page.
-- **Scroll to Anchor:** when clicking an item from the navigation menu, the link should scroll to the appropriate section.
+- [x] **Usability:** all features are usable across modern desktop, tablet, and phone browsers.
+- [x] **Navigation:** navigation is built dynamically as an unordered list.
+- [x] **Section Active State:** sections are highlighted when in view while scrolling through the page.
+- [x] **Scroll to Anchor:** when clicking an item from the navigation menu, the link scrolls to the appropriate section.
 
+## Get started
+1. Clone the GitHub repository to you computer.
+2. Open the `index.html` file into your browser.
+3. Enjoy.
+
+## License
+The **Udacity_Landing-page-progect** is free for the personal use.
--- js/app.js
+++ js/app.js
@@ -11,35 +11,35 @@
  * JS Standard: ESlint
  */
 
-//*************************
-// Define Global Variables
-//*************************
+/**************************
+ * Define Global Variables
+ **************************/
 
-const sections = document.querySelectorAll("section");
-const navList = document.getElementById("navbar__list");
+const sections = document.querySelectorAll('section');
+const navList = document.getElementById('navbar__list');
 
-// End Global Variables
-//*************************
+/* End Global Variables
+ ************************/
 
 
-//*************************
-// Start Helper Functions
-//*************************
+/**************************
+ * Start Helper Functions
+ **************************/
 
-//Function 'Add a CSS class (style) to an HTML element'
-//arguments: item = DOM element; style = string
+/* Function 'Add a CSS class (style) to an HTML element'
+   arguments: item = DOM element; style = string */
 const addToClassList = (item, style) => {
   return item.classList.add(style);
 };
 
-//Function 'Remove a CSS class (style) from an HTML element
-//arguments: item = DOM element; style = string
+/* Function 'Remove a CSS class (style) from an HTML element'
+   arguments: item = DOM element; style = string */
 const removeFromClassList = (item, style) => {
   return item.classList.remove(style);
 };
 
-//Function (returning boolean) to check if an element is partially visible in screen
-//argument: item = DOM element
+/* Function (returning boolean) to check if an element is partially visible in screen
+   argument: item = DOM element */
 const isInView = (item) => {
   const position = item.getBoundingClientRect();
   return (
@@ -48,31 +48,36 @@ const isInView = (item) => {
   );
 };
 
-// End Helper Functions
-//*************************
+/* End Helper Functions
+ **************************/
 
 
-//*************************
-// Begin Main Functions
-//*************************
+/**************************
+ * Begin Main Functions
+ **************************/
 
-//Build the navigation menu
+// Build the navigation menu
 const navBarMenu = (items) => {
-  //Loop over the list of sections to build navbar menu items
+  // Loop over the list of sections to build navbar menu items
   for (item of items) {
-    //Create list items
-    const navItem = document.createElement("li");
+    // Create list items
+    const navItem = document.createElement('li');
 
-    //Create link elements with "href", "class" and "id" attributes
-    const navItemLink = document.createElement("a");
+    // Create link elements with 'href', 'class' and 'id' attributes
+    const navItemLink = document.createElement('a');
       navItemLink.href = `#${item.id}`;
       navItemLink.id = `menu-${item.id}`;
-      addToClassList(navItemLink, "nav-link");
+      addToClassList(navItemLink, 'nav-link');
 
-    //Take menu items text from "data-nav" attributes
+    // Add event listener to menu links for 'click' event to scroll to the corresponding section
+    navItemLink.addEventListener('click', () => {
+      item.scrollIntoView({behavior: "smooth"});
+    });
+
+    // Take menu items text from 'data-nav' attributes of <section> tags
     const linkText = document.createTextNode(`${item.dataset.nav}`);
 
-    //Append elements: "menu item text" => <a> => <li> => <ul>
+    // Append elements: 'menu item text' => <a> => <li> => <ul>
     navItemLink.appendChild(linkText);
     navItem.appendChild(navItemLink);
     navList.appendChild(navItem);
@@ -80,42 +85,41 @@ const navBarMenu = (items) => {
 };
 
 
-//Function to add class '.active-section-highlighted'
-//to a section when top of it visible > 1/3 of viewport
-//argument: a list of elements
+/* Function to add class '.active-section-highlighted'
+ * to a section when top of it visible > 1/3 of viewport
+ * an argument: a list of elements */
 const sectionAndMenuActivator = (items) => {
-  window.addEventListener("scroll", () => {
-    //Define list of navbar menu items
-    const menuItems = document.getElementsByClassName("nav-link");
+  window.addEventListener('scroll', () => {
+    // Define list of navbar menu items
+    const menuItems = document.getElementsByClassName('nav-link');
 
-    //Remove "active-menu-link-highlighted" class from all menu items
+    // Remove 'active-menu-link-highlighted' class from all menu items
     for (menuItem of menuItems) {
-      removeFromClassList(menuItem, "active-menu-link-highlighted");
+      removeFromClassList(menuItem, 'active-menu-link-highlighted');
     };
 
     for (item of items) {
-      //Define current menu item (it corresponds to section-in-view)
+      // Define current menu item (it corresponds to section-in-view)
       const currentMenuItem = document.getElementById(`menu-${item.id}`);
 
-      //Add class "active-section-highlighted" to visible section 
-      //and corresponding menu item
+      // Add class 'active-section-highlighted' to visible section and corresponding menu item
       if (isInView(item)) {
-        addToClassList(item, "active-section-highlighted");
-        addToClassList(currentMenuItem, "active-menu-link-highlighted");
+        addToClassList(item, 'active-section-highlighted');
+        addToClassList(currentMenuItem, 'active-menu-link-highlighted');
       } else {
-        removeFromClassList(item, "active-section-highlighted");
+        removeFromClassList(item, 'active-section-highlighted');
       }
     }
   });
 };
 
-// End Main Functions
-//*************************
+/* End Main Functions
+ **************************/
 
 
-//*************************
-// Begin Events
-//*************************
+/**************************
+ * Begin Events
+ **************************/
 
 // Build menu
 navBarMenu(sections);
@@ -123,39 +127,47 @@ navBarMenu(sections);
 // Set sections as active
 sectionAndMenuActivator(sections);
 
+// Scroll to the header section when click on the logo
+const scrollToHeader = () => {
+  const logo = document.getElementById('navbar__logo');
+  logo.addEventListener('click', () => {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  });
+}
+scrollToHeader();
 
 
-//*************************
-// Mobile menu open/close
-//*************************
+/**************************
+ * Mobile menu open/close
+ **************************/
 
-const hamburgerIcon = document.getElementById("navbar__hamburger");
-const closeIcon = document.getElementById("navbar__close");
-const navListMenu = document.getElementById("navbar__list");
+const hamburgerIcon = document.getElementById('navbar__hamburger');
+const closeIcon = document.getElementById('navbar__close');
+const navListMenu = document.getElementById('navbar__list');
 
-//Open mobile navbar
-hamburgerIcon.addEventListener("click", () => {
+// Open mobile navbar
+hamburgerIcon.addEventListener('click', () => {
   // Set time out to see icons rotate CSS animation
   window.setTimeout(() => {
-    //Hide "hamburgermenu_icon.svg" by adding "display-none" CSS class
-    addToClassList(hamburgerIcon, "display-none");
-    //Show "close_icon.svg" by removing "display-none" CSS class
-    removeFromClassList(closeIcon, "display-none");
-    //Open mobile menu by removing "closed-nav" CSS class
-    removeFromClassList(navListMenu, "closed-nav");
+    // Hide 'hamburgermenu_icon.svg' by adding 'display-none' CSS class
+    addToClassList(hamburgerIcon, 'display-none');
+    // Show 'close_icon.svg' by removing 'display-none' CSS class
+    removeFromClassList(closeIcon, 'display-none');
+    // Open mobile menu by removing 'closed-nav' CSS class
+    removeFromClassList(navListMenu, 'closed-nav');
   }, 200);
 });
 
-//Close mobile navbar
-closeIcon.addEventListener("click", () => {
+// Close mobile navbar
+closeIcon.addEventListener('click', () => {
   // Set time out to see icons rotate animation
   window.setTimeout(() => {
-    //Hide "close_icon.svg" by adding "display-none" CSS class
-    addToClassList(closeIcon, "display-none");
-    //Show "hamburgermenu_icon.svg" by removing "display-none" CSS class
-    removeFromClassList(hamburgerIcon, "display-none");
-    //Close (hide) mobile menu by adding "closed-nav" CSS class
-    addToClassList(navListMenu, "closed-nav");
+    // Hide 'close_icon.svg' by adding 'display-none' CSS class
+    addToClassList(closeIcon, 'display-none');
+    // Show 'hamburgermenu_icon.svg' by removing 'display-none' CSS class
+    removeFromClassList(hamburgerIcon, 'display-none');
+    // Close (hide) mobile menu by adding 'closed-nav' CSS class
+    addToClassList(navListMenu, 'closed-nav');
   }, 200);
 });
 
